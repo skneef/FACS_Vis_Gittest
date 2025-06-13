224e4d6ddd06b3bb218b8cb1ba9f0131172b0746
StKn97 <102976099+StKn97@users.noreply.github.com>
2025-06-13 18:03:44 +0200

    test2

--- setup.py
+++ setup.py
@@ -30,13 +30,13 @@ def list_even(x):
 install_requirements = [req[x] for x in list_even(list(range(len(req))))]
 """
 
-install_reqs = ['anytree', 'bokeh', 'certifi', 'charset-normalizer', 'configparser', 'contourpy', 'cycler', 'FlowIO', 'FlowKit', 'FlowUtils', 'fonttools', 'idna', 'Jinja2', 'joblib', 'kiwisolver', 'lxml', 'MarkupSafe', 'matplotlib', 'networkx', 'numpy', 'packaging', 'pandas', 'pillow', 'psutil', 'psycopg2', 'PyMySQL', 'pyodbc', 'pyparsing', 'python-dateutil', 'python-hostlist', 'python-swiftclient', 'pytz', 'PyYAML', 'requests', 'sci', 'scikit-learn', 'scipy', 'seaborn', 'setuptools', 'six', 'threadpoolctl', 'tornado', 'tzdata', 'urllib3', 'xyzservices']
+install_reqs = ['anyio', 'anytree', 'argon2-cffi', 'argon2-cffi-bindings', 'arrow', 'asttokens', 'async-lru', 'attrs', 'babel', 'beautifulsoup4', 'bleach', 'bokeh', 'certifi', 'cffi', 'charset-normalizer', 'colorama', 'comm', 'configparser', 'contourpy', 'cycler', 'debugpy', 'decorator', 'defusedxml', 'executing', 'fastjsonschema', 'FlowIO', 'FlowKit', 'FlowUtils', 'fonttools', 'fqdn', 'h11', 'httpcore', 'httpx', 'idna', 'iniconfig', 'ipykernel', 'ipython', 'ipython_pygments_lexers', 'ipywidgets', 'isoduration', 'jedi', 'Jinja2', 'joblib', 'json5', 'jsonpointer', 'jsonschema', 'jsonschema-specifications', 'jupyter', 'jupyter-console', 'jupyter-events', 'jupyter-lsp', 'jupyter_client', 'jupyter_core', 'jupyter_server', 'jupyter_server_terminals', 'jupyterlab', 'jupyterlab_pygments', 'jupyterlab_server', 'jupyterlab_widgets', 'kiwisolver', 'lxml', 'MarkupSafe', 'matplotlib', 'matplotlib-inline', 'mistune', 'nbclient', 'nbconvert', 'nbformat', 'nest-asyncio', 'networkx', 'notebook', 'notebook_shim', 'numpy', 'overrides', 'packaging', 'pandas', 'pandocfilters', 'parso', 'pillow', 'platformdirs', 'pluggy', 'prometheus_client', 'prompt_toolkit', 'psutil', 'psycopg2', 'pure_eval', 'pycparser', 'Pygments', 'PyMySQL', 'pyodbc', 'pyparsing', 'pytest', 'python-dateutil', 'python-hostlist', 'python-json-logger', 'python-swiftclient', 'pytz', 'pywin32', 'pywinpty', 'PyYAML', 'pyzmq', 'referencing', 'requests', 'rfc3339-validator', 'rfc3986-validator', 'rpds-py', 'sci', 'scikit-learn', 'scipy', 'seaborn', 'Send2Trash', 'setuptools', 'six', 'sniffio', 'soupsieve', 'stack-data', 'tabulate', 'terminado', 'threadpoolctl', 'tinycss2', 'tornado', 'traitlets', 'types-python-dateutil', 'typing_extensions', 'tzdata', 'uri-template', 'urllib3', 'wcwidth', 'webcolors', 'webencodings', 'websocket-client', 'widgetsnbextension', 'xyzservices']
 
 
 # Setting up
 setup(
        # the name must match the folder name 'verysimplemodule'
-        name="FACS_Visualization_Package",
+        name="FACS_Visualization",
         version=VERSION,
         author="Stijn Kneefel",
         author_email="stijn.kneefel@outlook.com",
